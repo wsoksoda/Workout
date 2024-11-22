033d4b2e4f0cb6a9e63cf082a2d6f08154bd8983
wsoksoda <william.soksoda@ndus.edu>
2024-11-22 17:49:18 -0600

    [5-add-all-services] added services for every entity with getAll

---
+++ src/main/kotlin/demo/workout/service/MuscleGroupService.kt
@@ -0,0 +1,16 @@
+package demo.workout.service
+
+import demo.workout.entity.MuscleGroup
+import demo.workout.repository.MuscleGroupRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Service
+
+@Service
+class MuscleGroupService(private val muscleGroupRepository: MuscleGroupRepository) {
+    fun getAll(pageNumber: Int?, pageSize: Int?, sort: String?) : Page<MuscleGroup> {
+        val pageRequest = PageRequest.of(pageNumber?: 0, pageSize ?: 10, Sort.by(sort ?: "id"))
+        return muscleGroupRepository.findAll(pageRequest)
+    }
+}
---
+++ src/main/kotlin/demo/workout/service/WorkoutService.kt
@@ -0,0 +1,16 @@
+package demo.workout.service
+
+import demo.workout.entity.Workout
+import demo.workout.repository.WorkoutRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Service
+
+@Service
+class WorkoutService(private val workoutRepository: WorkoutRepository) {
+    fun getAll(pageNumber: Int?, pageSize: Int?, sort: String?) : Page<Workout> {
+        val pageRequest = PageRequest.of(pageNumber?: 0, pageSize ?: 10, Sort.by(sort ?: "id"))
+        return workoutRepository.findAll(pageRequest)
+    }
+}
