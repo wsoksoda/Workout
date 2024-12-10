8e1c59140a317b610853a623fd7bc40a0719a2fc
wsoksoda <william.soksoda@ndus.edu>
2024-12-10 17:53:42 -0600

    [28-connect-workout-groups-and-workouts] Fixed the @JoinTable annotation on WorkoutGroup entity

--- src/main/kotlin/demo/workout/entity/WorkoutGroup.kt
+++ src/main/kotlin/demo/workout/entity/WorkoutGroup.kt
@@ -8,11 +8,11 @@ data class WorkoutGroup(
     @Id
     val id: UUID,
     val name: String,
-    @OneToMany
+    @ManyToMany
     @JoinTable(
         name = "workout_workout_group",
-        joinColumns = [JoinColumn(name = "workout_id")],
-        inverseJoinColumns = [JoinColumn(name = "workout_group_id")]
+        joinColumns = [JoinColumn(name = "workout_group_id")],
+        inverseJoinColumns = [JoinColumn(name = "workout_id")]
     )
     val workouts: List<Workout>
 )
