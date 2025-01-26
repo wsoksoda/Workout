a6e6b183a47e9429023721ab78dd6b616c06acc1
wsoksoda <william.soksoda@ndus.edu>
2025-01-25 22:24:43 -0600

    [37] create a home page

--- src/main/resources/frontend/src/app/layout.tsx
+++ src/main/resources/frontend/src/app/layout.tsx
@@ -1,16 +1,17 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
-import {Provider} from "@/components/ui/provider"
+import { Provider } from "@/components/ui/provider"
 
-const geistSans = Geist({
+const geistSans = localFont({
+    src: "./fonts/GeistVF.woff",
     variable: "--font-geist-sans",
-    subsets: ["latin"],
+    weight: "100 900",
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+    src: "./fonts/GeistMonoVF.woff",
     variable: "--font-geist-mono",
-    subsets: ["latin"],
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
