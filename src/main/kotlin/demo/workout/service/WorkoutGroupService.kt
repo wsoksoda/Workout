70df0643ebf19140ea4f8ad2bdc776b382ebc3ac
wsoksoda <william.soksoda@ndus.edu>
2025-02-02 22:12:22 -0600

    [47] create a get by id endpoint

--- src/main/kotlin/demo/workout/service/WorkoutGroupService.kt
+++ src/main/kotlin/demo/workout/service/WorkoutGroupService.kt
@@ -6,12 +6,18 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class WorkoutGroupService(private val workoutGroupRepository: WorkoutGroupRepository) {
     fun getAll(pageNumber: Int?, pageSize: Int?, sort: String?) : Page<WorkoutGroup> {
         val pageRequest = PageRequest.of(pageNumber?: 0, pageSize ?: 10, Sort.by(sort ?: "id"))
         return workoutGroupRepository.findAll(pageRequest)
+    }
+
+    fun getById(id : UUID) : WorkoutGroup? {
+        return workoutGroupRepository.findById(id).getOrNull()
     }
 
     fun create(workoutGroups : List<WorkoutGroup>){
